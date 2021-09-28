3154238f4045fe0713c849ec4be51dfea7118e45
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-09-28 22:48:06 +0200

    Count NaN values and calculate statistics

--- explore_data.py
+++ explore_data.py
@@ -8,36 +8,12 @@ import load_data
 df = load_data.df
 
 # =============================================
-# AGE - Barplot
+# Count NaN values 
 # =============================================
-age = df["age"]
-
-# Count occurence of age 
-age_unique, counts = np.unique(age, return_counts=True)
-
-fig = plt.figure()
-ax1 = fig.add_subplot()
-ax1.bar(age_unique, counts)
-ax1.set_xlabel("age [years]")
-ax1.set_ylabel("count")
-# plt.show()
+print("Amount of NaN values")
+print(df.isna().sum())
 
 # =============================================
-# AGE and SEX - Barplot
+# Describe (statistcs) 
 # =============================================
-age_sex = df[["age", "sex"]]
-
-age_sex["sex"]
-age_sex_unique, as_counts = np.unique(age_sex["sex"], return_counts=True)
-# print(age_sex["sex"] == "Female")
-
-# age_sex.pivot("column","sex","age").plot(kind="bar")
-bar_width = 0.4
-
-male_age = age_sex[age_sex["sex"] == "Male"]
-female_age = age_sex[age_sex["sex"] == "Female"]
-
-male_unique, male_counts = np.unique(male_age["age"], return_counts=True)
-female_unique, female_counts = np.unique(female_age["age"], return_counts=True)
-age_unique, counts = np.unique(age, return_counts=True)
-
+print(df.describe())
