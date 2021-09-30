33fe83a5e890c9f06873b4d0044981ac2519ea60
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-09-30 15:17:37 +0200

    Create boxplots and histograms

--- explore_data.py
+++ explore_data.py
@@ -32,3 +32,34 @@ print(sdf.describe())
 # Print latex table 
 print("Latex table: Describe statistics")
 print(sdf.describe().to_latex())
+
+# # =============================================
+# # Write boxplots to file
+# # =============================================
+
+# Data to plot 
+data_cols = ["age", "capital-gain", "capital-loss", "hours-per-week"]
+
+# Loop through columns and make boxplot 
+for label in data_cols:
+    print("Creating box plot for label: " + label )
+    fig = px.box(df, y=label, points="all")
+    fig.write_image("plots/boxplot_"+ label +".jpg")
+
+
+# =============================================
+# Create histograms
+# =============================================
+
+# Age histogram colored by sex 
+print("Creating histogram for: age")
+fig = px.histogram(df, x = "age", color = "sex")
+fig.update_layout(bargap = 0.01)
+fig.write_image("plots/histogram_"+ "age" +".jpg")
+
+# Loop through labels and create histogram plot 
+for label in ["capital-gain", "capital-loss", "hours-per-week"]:
+    print("Creating histogram for: " + label)
+    fig = px.histogram(df, x = label)
+    fig.update_layout(bargap = 0.01)
+    fig.write_image("plots/histogram_"+ label +".jpg")
