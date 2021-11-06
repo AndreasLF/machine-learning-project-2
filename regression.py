271b7c321a006bbf26c7ce09d54962896066e785
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-11-06 17:54:45 +0100

    Linear regression with regularization added

--- regression.py
+++ regression.py
@@ -1,5 +1,7 @@
 from transform_data import *
 import sklearn.linear_model as lm
+from sklearn import model_selection
+
 
 # =============================================
 # Prepare data for regression
@@ -28,6 +30,7 @@ scaled_data = preprocessing.scale(X.values)
 scaled_X_df = pd.DataFrame(scaled_data, columns = X.columns)
 
 X = scaled_data
+y = y.to_numpy()
 
 # =============================================
 # Check if matrix is inversible 
@@ -43,6 +46,7 @@ else:
 I = np.around(np.matmul(xx,np.linalg.inv(xx)),decimals=0)
 print(I)
 # It has an inverse. No need to use regularization?
+
 
 # =============================================
 # Linear regression model with residuals plot
@@ -66,4 +70,217 @@ plt.ylabel('hours-per-week (estimated)');
 plt.subplot(2,1,2)
 plt.hist(residual,40)
 
-plt.show()
+
+plt.tight_layout()
+
+filename = 'plots2/linar_regression_residuals.jpg'
+if os.path.isfile(filename):
+   os.remove(filename)
+plt.savefig(filename)
+
+# =============================================
+# Regularization
+# =============================================
+def rlr_validate(X,y,lambdas,cvf=10):
+    ''' Validate regularized linear regression model using 'cvf'-fold cross validation.
+        Find the optimal lambda (minimizing validation error) from 'lambdas' list.
+        The loss function computed as mean squared error on validation set (MSE).
+        Function returns: MSE averaged over 'cvf' folds, optimal value of lambda,
+        average weight values for all lambdas, MSE train&validation errors for all lambdas.
+        The cross validation splits are standardized based on the mean and standard
+        deviation of the training set when estimating the regularization strength.
+        
+        Parameters:
+        X       training data set
+        y       vector of values
+        lambdas vector of lambda values to be validated
+        cvf     number of crossvalidation folds     
+        
+        Returns:
+        opt_val_err         validation error for optimum lambda
+        opt_lambda          value of optimal lambda
+        mean_w_vs_lambda    weights as function of lambda (matrix)
+        train_err_vs_lambda train error as function of lambda (vector)
+        test_err_vs_lambda  test error as function of lambda (vector)
+    '''
+    CV = model_selection.KFold(cvf, shuffle=True)
+    M = X.shape[1]
+    w = np.empty((M,cvf,len(lambdas)))
+    train_error = np.empty((cvf,len(lambdas)))
+    test_error = np.empty((cvf,len(lambdas)))
+    f = 0
+    y = y.squeeze()
+    for train_index, test_index in CV.split(X,y):
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        
+        # Standardize the training and set set based on training set moments
+        mu = np.mean(X_train[:, 1:], 0)
+        sigma = np.std(X_train[:, 1:], 0)
+        
+        X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+        X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
+        
+        # precompute terms
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
+        for l in range(0,len(lambdas)):
+            # Compute parameters for current value of lambda and current CV fold
+            # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+            lambdaI = lambdas[l] * np.eye(M)
+            lambdaI[0,0] = 0 # remove bias regularization
+            w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+            # Evaluate training and test performance
+            train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+            test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
+    
+        f=f+1
+
+    opt_val_err = np.min(np.mean(test_error,axis=0))
+    opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+    train_err_vs_lambda = np.mean(train_error,axis=0)
+    test_err_vs_lambda = np.mean(test_error,axis=0)
+    mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
+    
+    return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
+       
+# Add offset attribute
+X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+attributeNames = [u'Offset']+attribute_names
+M = M+1
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 5
+CV = model_selection.KFold(K, shuffle=True)
+#CV = model_selection.KFold(K, shuffle=False)
+
+# Values of lambda
+lambdas = np.power(10.,range(-2,12))
+
+# Initialize variables
+#T = len(lambdas)
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_rlr = np.empty((K,1))
+Error_test_rlr = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
+w_rlr = np.empty((M,K))
+mu = np.empty((K, M-1))
+sigma = np.empty((K, M-1))
+w_noreg = np.empty((M,K))
+
+k=0
+for train_index, test_index in CV.split(X,y):
+    
+    # extract training and test set for current CV fold
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    internal_cross_validation = 10    
+    
+    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+
+    # Standardize outer fold based on training set, and save the mean and standard
+    # deviations since they're part of the model (they would be needed for
+    # making new predictions) - for brevity we won't always store these in the scripts
+    mu[k, :] = np.mean(X_train[:, 1:], 0)
+    sigma[k, :] = np.std(X_train[:, 1:], 0)
+    
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+    
+    # Compute mean squared error without using the input data at all
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = opt_lambda * np.eye(M)
+    lambdaI[0,0] = 0 # Do no regularize the bias term
+    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
+
+    # Estimate weights for unregularized linear regression, on entire training set
+    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+    # Compute mean squared error without regularization
+    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+    #m = lm.LinearRegression().fit(X_train, y_train)
+    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    # Display the results for the last cross-validation fold
+    if k == K-1:
+        plt.figure(k, figsize=(12,8))
+        plt.subplot(1,2,1)
+        plt.semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Mean Coefficient Values')
+        plt.grid()
+        # You can choose to display the legend, but it's omitted for a cleaner 
+        # plot, since there are many attributes
+        plt.legend(attributeNames[1:], loc='best')
+        
+        plt.subplot(1,2,2)
+        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+        plt.loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Squared error (crossvalidation)')
+        plt.legend(['Train error','Validation error'])
+        plt.grid()
+    
+    # # To inspect the used indices, use these print statements
+    # print('Cross validation fold {0}/{1}:'.format(k+1,K))
+    # print('Train indices: {0}'.format(train_index))
+    # print('Test indices: {0}\n'.format(test_index))
+
+    k+=1
+
+plt.tight_layout()
+filename = 'plots2/regularization_error.jpg'
+
+if os.path.isfile(filename):
+   os.remove(filename)
+plt.savefig(filename)
+# plt.show()
+
+# Display results
+print('Linear regression without feature selection:')
+print('- Training error: {0}'.format(Error_train.mean()))
+print('- Test error:     {0}'.format(Error_test.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+print('Regularized linear regression:')
+print('- Training error: {0}'.format(Error_train_rlr.mean()))
+print('- Test error:     {0}'.format(Error_test_rlr.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
+
+
+print("Optimal lambda: ", opt_lambda)
+print()
+
+print('Weights in last fold:')
+table = pd.DataFrame(columns=["Attributes", "Model weights in las fold (with regularization)", "Weights (without regularization)"])
+table["Attributes"] = attributeNames
+table.index = table["Attributes"]
+table = table.drop("Attributes",axis=1)
+
+for m in range(M):
+    table["Model weights in las fold (with regularization)"][attributeNames[m]] = np.round(w_rlr[m,-1],2)
+    table["Weights (without regularization)"][attributeNames[m]] = np.round(w_noreg[m,-1],2)
+
+    # print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
+
+# Print latex table 
+print(table.to_latex())
