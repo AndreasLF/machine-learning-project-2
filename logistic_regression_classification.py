94c204755de19bdef93cb2e75b1203925d639afb
FenrisWulven <ollie.elmgreen1@gmail.com>
2021-11-09 17:07:50 +0100

    accuracy fixed for log

--- logistic_regression_classification.py
+++ logistic_regression_classification.py
@@ -7,6 +7,7 @@ from scipy.io import loadmat
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
+from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.dummy import DummyClassifier
 from toolbox_02450 import rocplot, confmatplot
@@ -31,21 +32,18 @@ for feature in features:
     le = preprocessing.LabelEncoder()
     X[feature] = le.fit_transform(X[feature])
 
-le = preprocessing.LabelEncoder()
-X[feature] = le.fit_transform(X[feature])
-# Standadizing the data with
+# Standardizing the data with
 scaled_data = preprocessing.scale(X.values)
 LABEL_ENCODED = pd.DataFrame(scaled_data, columns = X.columns)
 
 X = LABEL_ENCODED.to_numpy()
 print(X.shape,y.shape)
-print(N,M)
-print(C)
+print(N,M,C)
 
 # Create crossvalidation partition for evaluation
-# using stratification and 80/20 pct. split between respectively training and test 
+# using stratification and 90/10 pct. split between respectively training and test 
 K = 20
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, stratify=y)
 
 # Standardize the training and set set based on training set mean and std
 mu = np.mean(X_train, 0)
@@ -55,7 +53,7 @@ X_train = (X_train - mu) / sigma
 X_test = (X_test - mu) / sigma
 
 # Fit regularized logistic regression model to training data to predict the type of 
-lambda_interval = np.logspace(-6, 3, 50)
+lambda_interval = np.logspace(-6, 6, 50)
 train_error_rate = np.zeros(len(lambda_interval))
 test_error_rate = np.zeros(len(lambda_interval))
 coefficient_norm = np.zeros(len(lambda_interval))
@@ -73,12 +71,22 @@ for k in range(0, len(lambda_interval)):
     w_est = model.coef_[0] 
     coefficient_norm[k] = np.sqrt(np.sum(w_est**2))
 
+
+#Find optimal lambda value
 min_error = np.min(test_error_rate)
 opt_lambda_idx = np.argmin(test_error_rate)
 opt_lambda = lambda_interval[opt_lambda_idx]
 
+#model1 = LogisticRegression(penalty='l2', C=1/2023)
+model1 = LogisticRegression(penalty='l2', C=1/opt_lambda)
+model1.fit(X_train, y_train)
+y_pred_log = model1.predict(X_test)
+print("accuracy logistic: ", accuracy_score(y_pred_log, y_test))
 print("min error: ", min_error)
-print("acc_score: ", accuracy_score(y_test_est,y_test))
+
+#Denne bruger den nyeste lambdaværdi ved 10^6 som har en dårlig accuracy
+#y_test_est = model.predict(X_test)
+#print("accuracy logistic OG: ", accuracy_score(y_test_est, y_test))
 
 plt.figure(figsize=(8,8))
 #plt.plot(np.log10(lambda_interval), train_error_rate*100)
@@ -92,7 +100,7 @@ plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
 plt.ylabel('Error rate (%)')
 plt.title('Classification error')
 plt.legend(['Training error','Test error','Test minimum'],loc='upper right')
-plt.ylim([0, 40])
+plt.ylim([0, 30])
 plt.grid()
 plt.savefig("plots2/log_reg_classi_"+ "test_error" +".jpg")
 
@@ -115,7 +123,8 @@ y_train_est_base = baseline_clf.predict(X_train).T
 y_test_est_base = baseline_clf.predict(X_test).T
 
 print("\n base train predictions: ", y_train_est_base)
-print("baseline score: ", baseline_clf.score(X,y))
-print("baeline accuracy_score: ", accuracy_score(y_test_est_base,y_test))
+print("baseline training accuracy: ", baseline_clf.score(X,y))
+print("baseline accuracy accuracy: ", accuracy_score(y_test, y_test_est_base))
 
 
+ 
